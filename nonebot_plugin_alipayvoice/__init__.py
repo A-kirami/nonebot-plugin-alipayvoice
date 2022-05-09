125207cc2d74b29bb31f0ee6239bf4edb29d3083
Akirami <839592615@qq.com>
2022-05-09 23:54:22 +0800

    🎨 调整代码结构

--- nonebot_plugin_alipayvoice/__init__.py
+++ nonebot_plugin_alipayvoice/__init__.py
@@ -1,9 +1,8 @@
+import cn2an
 from nonebot import on_command
 from nonebot.adapters.onebot.v11 import Message, MessageSegment
 from nonebot.matcher import Matcher
 from nonebot.params import CommandArg
-
-import cn2an
 
 alipay_voice = on_command("支付宝到账", aliases={"支付宝语音"})
 
@@ -12,13 +11,12 @@ async def alipay(matcher: Matcher, args: Message = CommandArg()):
     amount = args.extract_plain_text().replace("元", "")
     if not amount:
         return
-    try:    
+    try:
         amount = cn2an.cn2an(amount, "smart")
-        if 0.01 <= amount <= 999999999999.99:
-            # url = f"https://mm.cqu.cc/share/zhifubaodaozhang/?money={amount}"
-            url = f"https://mm.cqu.cc/share/zhifubaodaozhang/mp3/{amount}.mp3"
-            await matcher.send(MessageSegment.record(url))
-        else:
-            await matcher.send("数字大小超出限制，支持范围为0.01~999999999999.99")
-    except:
-        await matcher.send("你tm输入的是数字吗？")
+    except Exception:
+        await matcher.send("错误的，请输入正确的数字")
+    if 0.01 <= amount <= 999999999999.99:
+        url = f"https://mm.cqu.cc/share/zhifubaodaozhang/mp3/{amount}.mp3"
+        await matcher.send(MessageSegment.record(url))
+    else:
+        await matcher.send("数字大小超出限制，支持范围为0.01~999999999999.99")
